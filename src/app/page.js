79e0d66867cb3cb5fdb4ffdabc9bf3290fdd95f2
kavishkakalhara1 <kavikalhara9h@gmail.com>
2026-06-14 12:28:39 +0530

    search bar added for job filtering

--- src/app/page.js
+++ src/app/page.js
@@ -140,7 +140,7 @@ export default function HomePage() {
               {' · '}
               <Link href="/terms" className="text-primary-200 hover:text-white hover:underline">Terms of Service</Link>
               {' · '}
-              <Link href="https://www.linkedin.com/in/kavishkakalhara/" className="text-primary-600 hover:underline">Developer</Link>
+              <Link href="https://www.linkedin.com/in/kavishkakalhara/" className="text-primary-200 hover:text-white hover:underline">Developer</Link>
             </p>
           </div>
         </div>
